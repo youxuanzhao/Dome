8e6c82830fd2028f36e3c34749247f3c0ee80b9a
Thunderw1nd <50003567+Thunderw1nd@users.noreply.github.com>
2022-06-27 12:08:39 +0800

    20220627
    BASIC REWORK

---
+++ Modules/PlayerController.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+export var MAX_SPEED = 150
+export var FRICTION = 100
+export var ACCELERATION = 20
+export var FACTOR = 1
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED * FACTOR , ACCELERATION * FACTOR)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO , FRICTION)
+	
+	move_and_collide(velocity * delta)
