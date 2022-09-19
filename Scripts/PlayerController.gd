f5b5723e7f92fa3493768d95a20f644ec964ed21
Thunderw1nd <50003567+Thunderw1nd@users.noreply.github.com>
2022-09-19 20:13:01 +0800

    Sep 19

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,7 +2,11 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var m_degree = 0.0
-var WeaponSpriteX = 0
+var PlayerScaleX
+var PlayerScaleY
+var WeaponSpriteX
+var WeaponScaleX
+var WeaponScaleY
 
 export var MAX_SPEED = 150
 export var FRICTION = 100
@@ -11,8 +15,21 @@ export var FACTOR = 1
 
 onready var PlayerSprite = $PlayerSprite
 onready var WeaponSprite = $WeaponSprite
+onready var PlayerAnimator = $AnimationPlayer
+
 func _ready():
+	PlayerAnimator.play("IDLE")
 	WeaponSpriteX = WeaponSprite.position.x
+	WeaponScaleX = WeaponSprite.scale.x
+	WeaponScaleY = WeaponSprite.scale.y
+	PlayerScaleX = PlayerSprite.scale.x
+	PlayerScaleY = PlayerSprite.scale.y
+	
+func toggle_visibility(TargetSprite):
+	if TargetSprite.visible == true:
+		TargetSprite.visible = false
+	elif TargetSprite.visible == false:
+		TargetSprite.visible = true
 func get_movement(velocity):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -23,16 +40,18 @@ func get_movement(velocity):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO , FRICTION)
 	return velocity
-func _physics_process(delta):
-	m_degree = get_global_mouse_position().angle_to_point(position)
+func rotate_sprite(WeaponSprite,PlayerSprite):
+	var m_degree = get_global_mouse_position().angle_to_point(position)
 	WeaponSprite.look_at(get_global_mouse_position())
 	if m_degree > -1.5 && m_degree < 1.5:
-		PlayerSprite.scale.x = 1
-		WeaponSprite.scale.y = 1
-		WeaponSprite.position.x = WeaponSpriteX
+		PlayerSprite.scale.x = PlayerScaleX * 1
+		WeaponSprite.scale.y = WeaponScaleY * 1
 	else:
-		PlayerSprite.scale.x = -1
-		WeaponSprite.scale.y = -1
-		WeaponSprite.position.x = WeaponSpriteX + 18
+		PlayerSprite.scale.x = PlayerScaleX * -1
+		WeaponSprite.scale.y = WeaponScaleY * -1
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_toggleweapon") == true:
+		toggle_visibility(WeaponSprite)
+	rotate_sprite(WeaponSprite,PlayerSprite)
 	velocity = get_movement(velocity)
 	move_and_slide(velocity)
